0431941776dd586a7d9e4417cb93821f81de9828
brian-b-tran <brian.tran0121@gmail.com>
2026-03-10 08:59:26 -0400

    Issue #14 & #15: Create category and expense table migrations - Create expense model - Create category model - use tempory fields for references that are not created yet - Run prisma migrate - Run prisma generate

---
+++ services/api/prisma/migrations/20260310125629_add_expense_and_category_tables/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "isDefault" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Expense" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "amountOriginal" DECIMAL(65,30) NOT NULL,
+    "currencyOriginal" TEXT NOT NULL,
+    "amountBase" DECIMAL(65,30),
+    "exchangeRateUsed" DECIMAL(65,30),
+    "categoryId" TEXT NOT NULL,
+    "budgetId" TEXT,
+    "recurringExpenseId" TEXT,
+    "notes" TEXT,
+    "date" TIMESTAMP(3) NOT NULL,
+    "time" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Expense_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
