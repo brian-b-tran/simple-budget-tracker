eeeed93492c925c5e1fe74ebab9448db05db0eb8
brian-b-tran <brian.tran0121@gmail.com>
2026-04-09 19:40:18 -0400

    Dashboard Screen Skeleton

--- apps/mobile/app/screens/DashboardScreen.tsx
+++ apps/mobile/app/screens/DashboardScreen.tsx
@@ -1,10 +1,58 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  ScrollView,
+  RefreshControl,
+} from 'react-native';
+import { useDashboard } from '../hooks/useDashboard';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function DashboardScreen() {
+  const {
+    activeBudgets,
+    recentExpenses,
+    upcomingReminders,
+    isLoading,
+    errorState,
+  } = useDashboard();
+
+  if (isLoading) {
+    return (
+      <SafeAreaProvider>
+        <SafeAreaView className=''>
+          <ActivityIndicator size='large' color='#00ff00' />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    );
+  }
+  if (errorState) {
+    return (
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <ScrollView refreshControl={<RefreshControl refreshing={true} />}>
+            <Text>An error occurred please refresh the page!</Text>
+            <Text>{errorState}</Text>
+          </ScrollView>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    );
+  }
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Dashboard</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} className='bg-slate-50'>
+      <View>
+        <Text>Dashboard</Text>
+      </View>
+      <View>
+        <Text>Recent Expenses</Text>
+      </View>
+      <View>
+        <Text>Budgets</Text>
+      </View>
+      <View>
+        <Text>Upcoming Reminders</Text>
+      </View>
+    </ScrollView>
   );
 }
